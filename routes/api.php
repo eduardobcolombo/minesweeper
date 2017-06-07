95f70e26537c9708c6747f61a1265c32a89b37aa
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2017-06-07 16:54:27 -0300

    added the library repository to persistence, start the Readme.md and some routes for test API

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('index', function () {
+        return response('show test game', 200)
+            ->header('Content-Type', 'json/application');
+    });
+
+    Route::post('game', function () {
+        return response()->json([
+            'username' => 'eduardo',
+            'game_id' => '9921',
+            'status' => 'playing',
+            'rows' => '10',
+            'cols' => '10',
+            'mines' => '3',
+            'board' => '',
+        ], 201)
+            ->header('Content-Type', 'json/application');
+    });
+
+});
