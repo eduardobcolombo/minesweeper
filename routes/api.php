bd025e18c69495982210d0207123a53c11660971
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2017-06-08 10:10:12 -0300

    add comments and update game_id

--- routes/api.php
+++ routes/api.php
@@ -26,11 +26,11 @@ Route::group(['prefix' => 'v1'], function () {
     Route::post('game', function () {
         return response()->json([
             'username' => 'eduardo',
-            'game_id' => '3',
+            'game_id' => 3,
             'status' => 'playing',
-            'rows' => '3',
-            'cols' => '3',
-            'bombs' => '1',
+            'rows' => 3,
+            'cols' => 3,
+            'bombs' => 1,
             'revealed'=> 'H,H,H,H,H,H,H,H,H'
         ], 201)
             ->header('Content-Type', 'json/application');
--- tests/Feature/GameTest.php
+++ tests/Feature/GameTest.php
@@ -11,6 +11,7 @@ use GuzzleHttp\Client;
 class GameTest extends TestCase
 {
     private $client = null;
+
     public function __construct() {
         // create our http client (Guzzle)
         $this->client = new Client([
@@ -23,7 +24,7 @@ class GameTest extends TestCase
     }
 
     /**
-     * A basic test example.
+     * Test if can get a new game
      *
      * @return void
      */
@@ -37,18 +38,21 @@ class GameTest extends TestCase
         ];
         $dataExpected = [
             'username' => 'eduardo',
-            'game_id' => 3,
+            'game_id' => 0,
             'status' => 'playing',
             'rows'=> 3,
             'cols'=> 3,
             'bombs'=> 1,
             'revealed'=> 'H,H,H,H,H,H,H,H,H'
         ];
-
+        // send request
         $response = $this->client->post('/api/v1/game', null, json_encode($data));
-
+        // check if header is json
         $this->assertEquals($response->getHeaders()['Content-Type'][0],'json/application');
         $data = json_decode($response->getBody(true), true);
+        // update game_id
+        $dataExpected['game_id'] = $data['game_id'];
+        // check if data is equal at expected
         $this->assertEquals($dataExpected, $data);
 
     }
